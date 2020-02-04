fd78df8e85eb7a8dda67457e34fd9e5283927b2d
FabricioLima-C <fabriciocabral07@gmail.com>
2020-02-04 20:20:37 -0300

    Handling captured pieces

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -1,7 +1,10 @@
 package Application;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -85,11 +88,31 @@ public class UI {
 		System.out.flush();
 	}
 	
-	public static void printMatch(ChessMatch chessMatch) {
+	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
+		System.out.println();
+		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: "+ chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+	}
+	
+	public static void printCapturedPieces(List<ChessPiece> captured) {
+		/*cria duas listas filtrando com expressão lambda para montar 
+		uma lista e peças brancas e outra de peças pretas*/
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		System.out.println("Captured pieces:");
+		System.out.print("White: ");
+		System.out.print(ANSI_WHITE);
+		//Método de imprimir arrays no java
+		System.out.println(Arrays.toString(white.toArray()));
+		System.out.print(ANSI_RESET);
+		
+		System.out.print("Black: ");
+		System.out.print(ANSI_YELLOW);
+		System.out.println(Arrays.toString(black.toArray()));
+		System.out.print(ANSI_RESET);
 	}
 
 }
